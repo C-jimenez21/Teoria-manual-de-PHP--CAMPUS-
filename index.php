7b6ef9a98c8d151fb03402a0e9364039d8be7ab7
C-jimenez21 <cristianjjc21@gmail.com>
2023-05-29 17:14:41 -0500

    feat: :tada: Inicializacion sobre teoria de PHP
    Este contenido se basa en el punto 5 del manual de PHP,  se estudia la configuracion basica de un porgrama con php

---
+++ index.php
@@ -0,0 +1,15 @@
+/**
+* ? Introduccion a php
+* todo Para llevar un orden cuando se trabaja en proyectos creados con php se recomienda las carpetas -css- -img- -js- -scrpits- -uploads- y el archivo principal index.php -> se recomienda esta nomenclatrua en el nombre del archivo php para que lo reconozca el servidor.
+* * Recordar la ruta de trabajo:  Computer/var/www/html
+*/
+
+
+/**
+* ? Estas son las etiquetas que indican el inicio y final de un script 
+*/
+<?php
+/**
+ * * Aqui va todo el codigo PHP
+*/
+?>
