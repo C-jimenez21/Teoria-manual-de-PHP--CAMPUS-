ff1522d22b09a020069ef561ba7f5f0a2f36c4ca
C-jimenez21 <cristianjjc21@gmail.com>
2023-06-22 00:14:40 -0500

    refactor: :recycle: Se estudian los metodos estaticos en PHP
    Se realiza un ejemplo con teoria sobre los metodos abstractos en clases

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 /**
- * ? Clases: Una clase puede tener sus propias constantes, variables (llamadas "propiedades"), y funciones (llamados "métodos").
+ * ? METODOS ESTATICOS
+ * * Este es un tipo de metodo que pertenece a la clase y no a la instancia especifica de la classe.
+ * todo A diferencia de los metodos normales, estos se pueden llamar directamente sin necesidad de crear una instancia de la clase.
+ * * Clase::metodoEstatico() -> estos solo pueden acceder a atributos estaticos, y la palabra $this se reemplaza por Self
  */
    
- echo "Ejemplo Clases";
+ echo "Ejemplo Metodos Estaticos";
  
  class Persona {
-   private $nombre;
-   protected $edad;
+   private string $nombre;
+   protected int $edad;
+   private static $nombreAux;
    
    public function __construct($nombre, $edad){
       $this->nombre = $nombre;
       $this->edad = $edad;
+      self::$nombreAux = $nombre;
    }
    public function getNombre(){
       return $this->nombre;
@@ -25,43 +30,11 @@
    public function setEdad($edad){
       $this->edad = $edad;
    }
-   private function saludar(){
-      echo "hola, mi nombre es ". $this->nombre;
+   public static function saludar(){
+      echo "hola, mi nombre es ". self::$nombreAux;
    }
  }
 
-   /**
-    * ? Como instanciar una clase
-    * * $variable = new Clase(args);
-    */
+ Persona::saludar();
 
-    $alumno = new Persona("Cristian" , 21);
-    $alumno -> getNombre();
-    $alumno -> getEdad();
-
-    /**
-     * ? creacion de una clase 2 haciendo uso del los atributos insertados en la version 8 de PHP
-     * * No hace falta declarar la variable antes del constructor sino en el mismo constructor.
-     */
-    class Persona {
-      public function __construct(private string $nombre, protected int $edad){
-         $this->nombre = $nombre;
-         $this->edad = $edad;
-      }
-      public function getNombre(){
-         return $this->nombre;
-      }
-      public function setNombre($nombre){
-         $this->nombre = $nombre;
-      }
-      public function getEdad(){
-         return $this->edad;
-      }
-      public function setEdad($edad){
-         $this->edad = $edad;
-      }
-      private function saludar(){
-         echo "hola, mi nombre es ". $this->nombre;
-      }
-    }
 ?>
